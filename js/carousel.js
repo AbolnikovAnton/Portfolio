f4601cf563d78de6572f2c700e32fd16fab8e93f
Anton <bakunin.abolnikov@gmail.com>
2020-06-06 22:07:58 +0300

    add autoplay for carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
     $('.carousel').carousel();
+
+    setInterval(function () {
+        $('.carousel').carousel('next');
+    }, 2500);
 });
 
 $('.carousel.carousel-slider').carousel({
     fullWidth: true,
     indicators: true,
 });
+
+
 
